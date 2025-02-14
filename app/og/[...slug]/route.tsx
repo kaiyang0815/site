9b0031a54d9ec61df13ca1fda0e4012daa35e894
kaiyang0815 <wangkaiyang0815@gmail.com>
2025-02-14 23:09:02 +0800

    Update OG image generation with TTF fonts and metadata configuration

--- app/og/[...slug]/route.tsx
+++ app/og/[...slug]/route.tsx
@@ -1,11 +1,12 @@
 import { metadataImage } from '@/lib/metadata-image';
 import { generateOGImage } from 'fumadocs-ui/og';
+import { type ImageResponse } from 'next/og';
 import { readFileSync } from 'node:fs';
 
-const font = readFileSync('./app/og/[...slug]/Inter-Regular.woff2');
-const fontBold = readFileSync('./app/og/[...slug]/Inter-Bold.woff2');
+const font = readFileSync('./app/og/[...slug]/Inter-Bold.ttf');
+const fontBold = readFileSync('./app/og/[...slug]/Inter-Regular.ttf');
 
-export const GET = metadataImage.createAPI((page) => {
+export const GET = metadataImage.createAPI((page): ImageResponse => {
   return generateOGImage({
     primaryTextColor: 'rgb(240,240,240)',
     title: page.data.title,
@@ -19,7 +20,7 @@ export const GET = metadataImage.createAPI((page) => {
       {
         name: 'Inter',
         data: fontBold,
-        weight: 700,
+        weight: 600,
       },
     ],
   });
